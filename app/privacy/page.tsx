9f29f09f3dce85d38c54cfb2587273ef409aeda0
ngacho <brandonngacho@gmail.com>
2024-04-23 19:07:40 -0400

    fix privacy policy and terms of service

---
+++ app/privacy/page.tsx
@@ -0,0 +1,387 @@
+import Navbar from "@/components/Navbar";
+
+export default function Privacy() {
+  return (
+    <>
+      <Navbar />
+      <div className="m-10">
+        <p>
+          <strong>Last updated:</strong> April 17th, 2024
+        </p>
+        <p>
+          This privacy notice for Email Digest (“<strong>we</strong>,” “
+          <strong>us</strong>,” or “<strong>our</strong>”), describes how and
+          why we might collect, store, use, and/or disclose (“
+          <strong>process</strong>”) your information when you use our services
+          (“<strong>Services</strong>”), such as when you:{" "}
+        </p>
+        <ul>
+          <li>
+            <p>
+              Visit our website at http://www.emaildigest.app, or any website
+              of ours that links to this privacy notice.{" "}
+            </p>
+          </li>
+          <li>
+            <p>
+              Download and use our add on (Email Digest), or any other
+              application of ours that links to this privacy notice (each, an “
+              <strong>App</strong>”).
+            </p>
+          </li>
+          <li>
+            <p>
+              Engage with our social media pages that link to this Privacy
+              Policy.{" "}
+            </p>
+          </li>
+          <li>
+            <p>
+              HTML-formatted email messages that we send to you that link to
+              this Privacy Policy.
+            </p>
+          </li>
+          <li>
+            <p>
+              Engage with us in other related ways, including any sales,
+              marketing, or events.{" "}
+            </p>
+          </li>
+        </ul>
+        <p>
+          <strong>Questions or concerns?</strong> Reading this privacy notice
+          will help you understand your privacy rights and choices. If you do
+          not agree with our policies and practices, please do not use our
+          Services. If you still have any questions or concerns, please contact
+          us at support-email-digest@protonmail.com.
+        </p>
+        <p>‍</p>
+        <p>
+          <strong>Table of Contents</strong>
+        </p>
+        <p>1. What Personal Information Do We Collect?</p>
+        <p>2. How We Use Personal Data</p>
+        <p>3. Information Automatically Collected</p>
+        <p>4. Third-Party Services</p>
+        <p>5. Choice</p>
+        <p>6. Storage and Security</p>
+        <p>7. Use of the Services By Minors</p>
+        <p>8. Account Information</p>
+        <p>9. Jurisdiction and Cross-Border Transfer</p>
+        <p>10. Sensitive Information</p>
+        <p>11. Third-Party Payment Service</p>
+        <p>12. California Residents</p>
+        <p>13. Updates To This Notice</p>
+        <p>14. How Can You Contact Us About This Notice?</p>
+        <p>‍</p>
+        <p>
+          <strong>1. What Personal Information Do We Collect?</strong>{" "}
+        </p>
+        <p>
+          Email Data: we collect your email address and the contents of any
+          emails and messages you draft or receive in our Service, as well as
+          associated metadata (such as the time sent).
+        </p>
+        <p>
+          Google Data: we will sync with your Gmail account so we can provide
+          the Service. Through this integration, we will have access to your
+          name, profile picture, and your Gmail inbox and any information
+          available there, including the contents of your emails (&quot;Google
+          Data&quot;). Notwithstanding anything else in this Privacy Policy, we
+          (1) only use the Google Data to provide the Service; (2) do not
+          transfer Google Data to third parties except as necessary to provide
+          or improve the Service, as required by law, or in connection with a
+          merger, acquisition, or sale of assets where we provide notice to
+          users; (3) do not use the Google Data for serving advertisements; and
+          (4) do not permit humans to read Google Data, except (a) if we obtain
+          your affirmative consent, (b) as necessary for security purposes or to
+          comply with applicable law, or (c) our use is limited to internal
+          operations and the Google Data (including derivations) have been
+          aggregated and anonymized
+        </p>
+        <p>
+          <strong>2. How We Use Personal Data</strong>
+        </p>
+        <p>We may use Personal Data for the following purposes:</p>
+        <ul>
+          <li>
+            <p>
+              To facilitate account creation and authentication and otherwise
+              manage user accounts. <em>**</em>We may process your information,
+              so that you can create and log into your account, as well as keep
+              your account in working order.{" "}
+            </p>
+          </li>
+          <li>
+            <p>
+              To provide the Service, including sending and viewing email data,
+              organizing your email data, and seeing information about your
+              contacts;
+            </p>
+          </li>
+          <li>
+            <p>
+              To respond to your inquiries, comments, feedback, or questions;
+            </p>
+          </li>
+          <li>
+            <p>
+              To evaluate and improve our Services, products, marketing, and
+              your experience. <em>**</em>We may process your information when
+              we believe it is necessary to identify usage trends, determine the
+              effectiveness of our promotional campaigns, and to evaluate and
+              improve our Services, products, marketing, and your experience.{" "}
+            </p>
+          </li>
+          <li>
+            <p>
+              To solicit referrals for new users from current users and to
+              contact such referrals and other potential users;
+            </p>
+          </li>
+          <li>
+            <p>
+              To send administrative information to you, for example,
+              information regarding the Service and changes to our terms,
+              conditions, and policies;
+            </p>
+          </li>
+          <li>
+            <p>To analyze how you interact with our Service;</p>
+          </li>
+          <li>
+            <p>To maintain and improve the Service;</p>
+          </li>
+          <li>
+            <p>To develop new products and services;</p>
+          </li>
+          <li>
+            <p>
+              To prevent fraud, criminal activity, or misuses of our Service,
+              and to ensure the security of our IT systems, architecture, and
+              networks; and
+            </p>
+          </li>
+          <li>
+            <p>
+              To comply with legal obligations and legal process and to protect
+              our rights, privacy, safety, or property, and/or that of our
+              affiliates, you, or other third parties.
+            </p>
+          </li>
+        </ul>
+        <p>
+          <strong>
+            3. When and With Whom Do We Share Your Personal Information?
+          </strong>{" "}
+        </p>
+        <p>We disclose your Personal Information: </p>
+        <ul>
+          <li>
+            <p>
+              <strong>Through the functionality you request.</strong> When you
+              use the Services, including to interact with third parties, you
+              will share, direct us to share, or consent to our sharing
+              information relevant to the requested service with those third
+              parties. We use third-party large language models (LLMs) to
+              support some of our functionality. Depending on the functionality
+              you select, your Personal Information may be shared with
+              third-party tools, including LLMs. For example, if you instruct
+              the Services to research a topic using a particular third-party
+              service, the search terms will be shared with the selected
+              third-party service. Please see the “Third-Party Services” section
+              below. Your information also may be shared with third parties if
+              you use the Services to schedule a meeting, which will require us
+              to disclose Personal Information relevant to the scheduling to
+              other attendees in accordance with your instructions.
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>In connection with business transfers.</strong> We may
+              share or transfer your information in connection with, or during
+              negotiations of, reorganization, merger, sale, joint venture,
+              assignment, transfer, or other disposition of all or any portion
+              of our assets or stock (including in connection with any
+              bankruptcy or similar proceedings).{" "}
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong>Google API Services User Data Policy.</strong> Email
+              Digest AI’s use and transfer to any other app of information
+              received from Google APIs will adhere to
+              <a href="https://developers.google.com/terms/api-services-user-data-policy#additional_requirements_for_specific_api_scopes">
+                {" "}
+                Google API Services User Data Policy
+              </a>
+              , including the Limited Use requirements.{" "}
+            </p>
+          </li>
+        </ul>
+        <p>‍</p>
+        <p>
+          <strong>4. Third-Party Services</strong>
+        </p>
+        <p>
+          This Privacy Policy does not address, and we are not responsible for,
+          the privacy, information, or other practices of any third parties,
+          including any third party operating any website or service to which
+          the Services link. The inclusion of a link on the Services does not
+          imply endorsement of the linked site or service by us or by our
+          affiliates.{" "}
+        </p>
+        <p>
+          In addition, we are not responsible for the information collection,
+          use, disclosure, or security policies or practices of other
+          organizations, such as Amazon, Facebook, Apple, Google, Microsoft,
+          RIM, or any other app developer, app provider, social media platform
+          provider, operating system provider, wireless service provider, or
+          device manufacturer, including with respect to any Personal
+          Information that you disclose to other organizations through or in
+          connection with the Apps or our Social Media Pages.
+        </p>
+        <p>‍</p>
+        <p>
+          <strong>5. Choice</strong>
+        </p>
+        <p>
+          You may opt out from receiving marketing-related emails from us. If
+          you no longer want to receive marketing-related emails from us on a
+          going-forward basis, you may opt out by following the unsubscribe
+          instructions in the email received or by contacting us at the
+          information listed in the _“_How Can You Contact Us About This
+          Notice?” section below.
+        </p>
+        <p>
+          We will comply with your request(s) as soon as reasonably practicable.
+          Please note that if you opt out of receiving marketing-related emails
+          from us, we may still send you important administrative messages, from
+          which you cannot opt out.
+        </p>
+        <p>‍</p>
+        <p>
+          <strong>6. Storage and Security</strong>{" "}
+        </p>
+        <p>
+          You use the Service at your own risk. We implement commercially
+          reasonable technical, administrative, and organizational measures to
+          protect Personal Data both online and offline from loss, misuse, and
+          unauthorized access, disclosure, alteration, or destruction. Our
+          systems and policies have undergone a Google Security Review. This
+          security audit verifies that we are capable of handling data securely
+          and deleting user data upon user request.
+        </p>
+        <p>
+          However, no Internet or email transmission is ever fully secure or
+          error free. In particular, email sent to or from us may not be secure.
+          Therefore, you should take special care in deciding what information
+          you send to us via the Service or email. Please keep this in mind when
+          disclosing any Personal Data to Shortwave via the Internet. In
+          addition, we are not responsible for circumvention of any privacy
+          settings or security measures contained on the Service, or third party
+          websites.
+        </p>
+        <p>‍We don’t keep personal information, other third parties might.</p>
+        <p>
+          <strong>7. Use of the Services By Minors</strong>
+        </p>
+        <p>
+          Our Service is not directed to children who are under the age of 13.
+          Shortwave does not knowingly collect Personal Data from children under
+          the age of 13. If you have reason to believe that a child under the
+          age of 13 has provided Personal Data to Shortwave through the Service
+          please contact us and we will endeavor to delete that information from
+          our databases.
+        </p>
+        <p>
+          <strong>8. Account Information</strong>{" "}
+        </p>
+        <p>
+          You can log into your user account at any time to review or change the
+          information in your account or to terminate your account.{" "}
+        </p>
+        <p>‍</p>
+        <p>
+          <strong>9. Jurisdiction and Cross-Border Transfer</strong>
+        </p>
+        <p>
+          We are located in the United States. We may store and process your
+          Personal Information in any country where we have facilities or in
+          which we engage service providers. By using the Services, you
+          understand that your information will be transferred to countries
+          outside of your country of residence, including the United States,
+          which may have data protection rules that are different from those of
+          your country.{" "}
+        </p>
+        <p>‍</p>
+        <p>
+          ‍<strong>10. Sensitive Information</strong>
+        </p>
+        <p>
+          Unless we request it, we ask that you not send us, and you not
+          disclose, any sensitive Personal Information (<em>e.g.</em>, Social
+          Security numbers, information related to racial or ethnic origin,
+          political opinions, religion or other beliefs, health, biometrics or
+          genetic characteristics, or criminal background) on or through the
+          Services or otherwise to us.
+        </p>
+        <p>‍</p>
+        <p>
+          <strong>11. Third-Party Payment Service</strong>
+        </p>
+        <p>
+          The Services may provide functionality allowing you to make payments
+          using a third-party payment service with which you have created your
+          own account. When you use such a service to make a payment to us, your
+          Personal Information will be collected by such third party and not by
+          us and will be subject to the third party’s privacy policy, rather
+          than this Privacy Policy. We have no control over, and are not
+          responsible for, any such third party’s collection, use, or disclosure
+          of your Personal Information.
+        </p>
+        <p>‍</p>
+        <p>
+          <strong>12. California Residents</strong>{" "}
+        </p>
+        <p>
+          If you are California resident and would like to request and obtain
+          from us information about categories of Personal Information (if any)
+          we disclosed to third parties for direct marketing purposes and the
+          names and addresses of all third parties with which we shared Personal
+          Information in the immediately preceding calendar year, please submit
+          your request in writing to us using the contact information provided
+          below.{" "}
+        </p>
+        <p>
+          If you are under 18 years of age, reside in California, and have a
+          registered account with the Services, you have the right to request
+          removal of unwanted data that you publicly post on the Services. To
+          request removal of such data, please contact us using the contact
+          information provided below and include the email address associated
+          with your account and a statement that you reside in California. We
+          will make sure the data is not publicly displayed on the Services, but
+          please be aware that the data may not be completely or comprehensively
+          removed from all our systems (<em>e.g.</em>, backups, etc.).{" "}
+        </p>
+        <p>‍</p>
+        <p>
+          ‍<strong>13. Updates To This Notice</strong>{" "}
+        </p>
+        <p>
+          We may update this privacy notice from time to time. The updated
+          version will be indicated by an updated “Revised” date and the updated
+          version will be effective as soon as it is accessible.{" "}
+        </p>
+        <p>‍</p>
+        <p>
+          <strong>14. How Can You Contact Us About This Notice?</strong>{" "}
+        </p>
+        <p>
+          If you have questions or comments about this notice, you may email us
+          at support-email-digest@protonmail.com
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ app/terms-of-service/page.tsx
@@ -0,0 +1,141 @@
+import Navbar from "@/components/Navbar";
+
+export default function Privacy() {
+  return (
+    <>
+      <Navbar />
+      <div className="m-10">
+        <h1 id="-website-terms-and-conditions-of-use-a-id-website-terms-and-conditions-of-use-a-">
+          <strong>Website Terms and Conditions of Use</strong>
+          <a id="website-terms-and-conditions-of-use"></a>
+        </h1>
+        <h2 id="-1-terms-a-id-1-terms-a-">
+          <strong>1. Terms</strong>
+          <a id="1-terms"></a>
+        </h2>
+        <p>
+          By accessing this Website, accessible from www.emaildigest.app, you
+          are agreeing to be bound by these Website Terms and Conditions of Use
+          and agree that you are responsible for the agreement with any
+          applicable local laws. If you disagree with any of these terms, you
+          are prohibited from accessing this site. The materials contained in
+          this Website are protected by copyright and trade mark law.
+        </p>
+        <h2 id="-2-use-license-a-id-2-use-license-a-">
+          <strong>2. Use License</strong>
+          <a id="2-use-license"></a>
+        </h2>
+        <p>
+          Permission is granted to temporarily download one copy of the
+          materials on Email Digest&#39;s Website for personal, non-commercial
+          transitory viewing only. This is the grant of a license, not a
+          transfer of title, and under this license you may not:
+        </p>
+        <ul>
+          <li>
+            <p>modify or copy the materials;</p>
+          </li>
+          <li>
+            <p>
+              use the materials for any commercial purpose or for any public
+              display;
+            </p>
+          </li>
+          <li>
+            <p>
+              attempt to reverse engineer any software contained on Email
+              Digest&#39;s Website;
+            </p>
+          </li>
+          <li>
+            <p>
+              remove any copyright or other proprietary notations from the
+              materials; or
+            </p>
+          </li>
+          <li>
+            <p>
+              transferring the materials to another person or &quot;mirror&quot;
+              the materials on any other server.
+            </p>
+          </li>
+        </ul>
+        <p>
+          This will let Email Digest to terminate upon violations of any of
+          these restrictions. Upon termination, your viewing right will also be
+          terminated and you should destroy any downloaded materials in your
+          possession whether it is printed or electronic format.
+        </p>
+        <h2 id="-3-disclaimer-a-id-3-disclaimer-a-">
+          <strong>3. Disclaimer</strong>
+          <a id="3-disclaimer"></a>
+        </h2>
+        <p>
+          All the materials on Email Digest&#39;s Website are provided &quot;as
+          is&quot;. Email Digest makes no warranties, may it be expressed or
+          implied, therefore negates all other warranties. Furthermore, Email
+          Digest does not make any representations concerning the accuracy or
+          reliability of the use of the materials on its Website or otherwise
+          relating to such materials or any sites linked to this Website.
+        </p>
+        <h2 id="-4-limitations-a-id-4-limitations-a-">
+          <strong>4. Limitations</strong>
+          <a id="4-limitations"></a>
+        </h2>
+        <p>
+          Email Digest or its suppliers will not be hold accountable for any
+          damages that will arise with the use or inability to use the materials
+          on Email Digest&#39;s Website, even if Email Digest or an authorized
+          representative of this Website has been notified, orally or written,
+          of the possibility of such damage. Some jurisdiction does not allow
+          limitations on implied warranties or limitations of liability for
+          incidental damages, these limitations may not apply to you.
+        </p>
+        <h2 id="-5-revisions-and-errata-a-id-5-revisions-and-errata-a-">
+          <strong>5. Revisions and Errata</strong>
+          <a id="5-revisions-and-errata"></a>
+        </h2>
+        <p>
+          The materials appearing on Email Digest&#39;s Website may include
+          technical, typographical, or photographic errors. Email Digest will
+          not promise that any of the materials in this Website are accurate,
+          complete, or current. Email Digest may change the materials contained
+          on its Website at any time without notice. Email Digest does not make
+          any commitment to update the materials.
+        </p>
+        <h2 id="-6-links-a-id-6-links-a-">
+          <strong>6. Links</strong>
+          <a id="6-links"></a>
+        </h2>
+        <p>
+          Email Digest has not reviewed all of the sites linked to its Website
+          and is not responsible for the contents of any such linked site. The
+          presence of any link does not imply endorsement by Email Digest of the
+          site. The use of any linked website is at the user&#39;s own risk.
+        </p>
+        <h2 id="-7-site-terms-of-use-modifications-a-id-7-site-terms-of-use-modifications-a-">
+          <strong>7. Site Terms of Use Modifications</strong>
+          <a id="7-site-terms-of-use-modifications"></a>
+        </h2>
+        <p>
+          Email Digest may revise these Terms of Use for its Website at any time
+          without prior notice. By using this Website, you are agreeing to be
+          bound by the current version of these Terms and Conditions of Use.
+        </p>
+        <h2 id="-8-your-privacy-a-id-8-your-privacy-a-">
+          <strong>8. Your Privacy</strong>
+          <a id="8-your-privacy"></a>
+        </h2>
+        <p>Please read our Privacy Policy.</p>
+        <h2 id="-9-governing-law-a-id-9-governing-law-a-">
+          <strong>9. Governing Law</strong>
+          <a id="9-governing-law"></a>
+        </h2>
+        <p>
+          Any claim related to Email Digest&#39;s Website shall be governed by
+          the laws of af without regards to its conflict of law provisions.
+        </p>
+      </div>
+    </>
+  );
+}
