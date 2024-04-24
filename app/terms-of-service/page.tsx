5087e98b1c098f88ad2075820c1724f218cccd53
ngacho <brandonngacho@gmail.com>
2024-04-23 20:19:21 -0400

    fix terms of service

--- app/terms-of-service/page.tsx
+++ app/terms-of-service/page.tsx
@@ -4,136 +4,156 @@ export default function Privacy() {
   return (
     <>
       <Navbar />
-      <div className="m-10">
-        <h1 id="-website-terms-and-conditions-of-use-a-id-website-terms-and-conditions-of-use-a-">
-          <strong>Website Terms and Conditions of Use</strong>
-          <a id="website-terms-and-conditions-of-use"></a>
-        </h1>
-        <h2 id="-1-terms-a-id-1-terms-a-">
-          <strong>1. Terms</strong>
-          <a id="1-terms"></a>
-        </h2>
+      <div className="mx-20 my-10">
+        <h1 className="text-4xl font-extrabold ">Terms of Service</h1>
+        <h2 className="text-2xl font-bold ">Welcome to Email Digest</h2>
         <p>
-          By accessing this Website, accessible from www.emaildigest.app, you
-          are agreeing to be bound by these Website Terms and Conditions of Use
-          and agree that you are responsible for the agreement with any
-          applicable local laws. If you disagree with any of these terms, you
-          are prohibited from accessing this site. The materials contained in
-          this Website are protected by copyright and trade mark law.
+          Thank you for choosing Email Digest for your email summarization
+          needs. By accessing this website, and any services we provide
+          including but not limited to Email Digest Add On, you are agreeing to
+          be bound by these Website Terms and Conditions of Use and agree that
+          you are responsible for the agreement with any applicable local laws.
+          If you disagree with any of these terms, you are prohibited from
+          accessing this site. We (email digest) provide an add on that is
+          powered by large language models hosted by OpenAI to provide concise
+          summaries of your emails, enhancing your productivity and email
+          management. By using our services, you agree to be bound by these
+          Terms of Service.
         </p>
-        <h2 id="-2-use-license-a-id-2-use-license-a-">
-          <strong>2. Use License</strong>
-          <a id="2-use-license"></a>
-        </h2>
+        <h2 className="text-2xl font-bold ">Services Provided</h2>
         <p>
-          Permission is granted to temporarily download one copy of the
-          materials on Email Digest&#39;s Website for personal, non-commercial
-          transitory viewing only. This is the grant of a license, not a
-          transfer of title, and under this license you may not:
+          Email Digest provides an email summarization tool that interfaces with
+          Open AI’s technology to generate summaries of the content you select.
+          Our tool aims to provide accurate and efficient summaries to help you
+          manage your correspondence more effectively.
         </p>
-        <ul>
-          <li>
-            <p>modify or copy the materials;</p>
-          </li>
-          <li>
-            <p>
-              use the materials for any commercial purpose or for any public
-              display;
-            </p>
-          </li>
-          <li>
-            <p>
-              attempt to reverse engineer any software contained on Email
-              Digest&#39;s Website;
-            </p>
-          </li>
-          <li>
-            <p>
-              remove any copyright or other proprietary notations from the
-              materials; or
-            </p>
-          </li>
-          <li>
-            <p>
-              transferring the materials to another person or &quot;mirror&quot;
-              the materials on any other server.
-            </p>
-          </li>
-        </ul>
+        <h2 className="text-2xl font-bold ">Acceptance of Terms</h2>
         <p>
-          This will let Email Digest to terminate upon violations of any of
-          these restrictions. Upon termination, your viewing right will also be
-          terminated and you should destroy any downloaded materials in your
-          possession whether it is printed or electronic format.
+          Your access to and use of the Service is conditioned upon your
+          acceptance of and compliance with these Terms. These Terms apply to
+          all visitors, users and others who wish to access or use the Service.
+          By accessing or using the Service you agree to be bound by these
+          Terms. If you disagree with any part of the terms then you do not have
+          permission to access the Service.
         </p>
-        <h2 id="-3-disclaimer-a-id-3-disclaimer-a-">
-          <strong>3. Disclaimer</strong>
-          <a id="3-disclaimer"></a>
-        </h2>
+        <h2 className="text-2xl font-bold ">Changes to Terms of Service</h2>
         <p>
-          All the materials on Email Digest&#39;s Website are provided &quot;as
-          is&quot;. Email Digest makes no warranties, may it be expressed or
-          implied, therefore negates all other warranties. Furthermore, Email
-          Digest does not make any representations concerning the accuracy or
-          reliability of the use of the materials on its Website or otherwise
-          relating to such materials or any sites linked to this Website.
+          We reserve the right to modify these terms at any time. We will notify
+          you of any changes by posting the new Terms of Service on this page.
+          Your continued use of our services after such changes constitutes your
+          agreement to the new terms.
         </p>
-        <h2 id="-4-limitations-a-id-4-limitations-a-">
-          <strong>4. Limitations</strong>
-          <a id="4-limitations"></a>
-        </h2>
+        <h2 className="text-2xl font-bold ">Data Handling</h2>
         <p>
-          Email Digest or its suppliers will not be hold accountable for any
-          damages that will arise with the use or inability to use the materials
-          on Email Digest&#39;s Website, even if Email Digest or an authorized
-          representative of this Website has been notified, orally or written,
-          of the possibility of such damage. Some jurisdiction does not allow
-          limitations on implied warranties or limitations of liability for
-          incidental damages, these limitations may not apply to you.
+          We respect your privacy and commit to handling your data responsibly.
+          When you use our service, your email data is sent to Open AI’s servers
+          where the summarization process occurs. We do not store copies of your
+          emails and only retain minimal data necessary to provide our services
+          to you.
         </p>
-        <h2 id="-5-revisions-and-errata-a-id-5-revisions-and-errata-a-">
-          <strong>5. Revisions and Errata</strong>
-          <a id="5-revisions-and-errata"></a>
-        </h2>
+        <h2 className="text-2xl font-bold ">Data Mismanagement Disclaimer</h2>
         <p>
-          The materials appearing on Email Digest&#39;s Website may include
-          technical, typographical, or photographic errors. Email Digest will
-          not promise that any of the materials in this Website are accurate,
-          complete, or current. Email Digest may change the materials contained
-          on its Website at any time without notice. Email Digest does not make
-          any commitment to update the materials.
+          While OpenAI is a trusted partner, Email Digest does not control the
+          data management practices of third parties, including OpenAI. We
+          cannot be held responsible for any mismanagement, misuse, or
+          mishandling of data by OpenAI. or any other third-party service
+          providers. We encourage you to review Open AI’s privacy policies,
+          available{" "}
+          <a href="https://openai.com/policies/privacy-policy">here</a> to
+          understand their data handling practices.
         </p>
-        <h2 id="-6-links-a-id-6-links-a-">
-          <strong>6. Links</strong>
-          <a id="6-links"></a>
-        </h2>
+        <h2 className="text-2xl font-bold ">User Responsibilities</h2>
         <p>
-          Email Digest has not reviewed all of the sites linked to its Website
-          and is not responsible for the contents of any such linked site. The
-          presence of any link does not imply endorsement by Email Digest of the
-          site. The use of any linked website is at the user&#39;s own risk.
+          As a user, you agree not to use our services to process sensitive or
+          highly confidential information. You are responsible for ensuring that
+          your use of our services complies with applicable laws and
+          regulations.
         </p>
-        <h2 id="-7-site-terms-of-use-modifications-a-id-7-site-terms-of-use-modifications-a-">
-          <strong>7. Site Terms of Use Modifications</strong>
-          <a id="7-site-terms-of-use-modifications"></a>
-        </h2>
+        <h2 className="text-2xl font-bold ">Limitation of Liability</h2>
         <p>
-          Email Digest may revise these Terms of Use for its Website at any time
-          without prior notice. By using this Website, you are agreeing to be
-          bound by the current version of these Terms and Conditions of Use.
+        Email Digest will not be
+          liable for any direct, indirect, incidental, special, consequential,
+          or exemplary damages, including but not limited to damages for loss of
+          profits, goodwill, use, data, or other intangible losses resulting
+          from the use of our services or third-party products like those of
+          Open AI.
         </p>
-        <h2 id="-8-your-privacy-a-id-8-your-privacy-a-">
-          <strong>8. Your Privacy</strong>
-          <a id="8-your-privacy"></a>
-        </h2>
-        <p>Please read our Privacy Policy.</p>
-        <h2 id="-9-governing-law-a-id-9-governing-law-a-">
-          <strong>9. Governing Law</strong>
-          <a id="9-governing-law"></a>
-        </h2>
         <p>
-          Any claim related to Email Digest&#39;s Website shall be governed by
-          the laws of af without regards to its conflict of law provisions.
+          Email Digest will not be held accountable for any damages that will
+          arise with the use or inability to use the materials on Email
+          Digest&#39;s Website, Email Digest’s Add on even if Email Digest or an
+          authorized representative of this Website has been notified, orally or
+          written, of the possibility of such damage. Some jurisdiction does not
+          allow limitations on implied warranties or limitations of liability
+          for incidental damages, these limitations may not apply to you.
+        </p>
+        {/*
+        <p>
+           <h2 className="text-2xl font-bold ">Usage Based Billing</h2>{" "}
+        </p>
+        <p>
+          Some parts of the Service are billed on a usage basis. You will be
+          billed based on usage from the previous (&quot;Billing Cycle&quot;).
+          Billing cycles are set on a monthly basis. At the end of each Billing
+          Cycle, your subscription will automatically renew under the exact same
+          conditions unless you cancel it or Email DIgest cancels it. You may
+          cancel at any time either through your online account management page
+          or by contacting Email Digest customer support team.
+        </p>
+        <h2 className="text-2xl font-bold ">Free Trial and Fee Changes</h2>
+        <p>
+           
+        </p>
+        <p>
+          Email Digest may, at its sole discretion, offer a Free Trial for a
+          limited period of time. If you enter your billing information when
+          signing up for the Free Trial, you will not be charged by Email Digest
+          until the Free Trial has expired. On the last day of the Free Trial
+          period, unless you cancel, you will be automatically charged for the
+          applicable usage fees. Email Digest, in its sole discretion and at any
+          time, may modify the usage fees. Any usage fee change will become
+          effective at the end of the then-current Billing Cycle.
+        </p>
+        */}
+        <h2 className="text-2xl font-bold ">Termination</h2>
+        <p>
+           
+        </p>
+        {/*
+        <p>
+          We may terminate or suspend your account and bar access to the Service
+          immediately, without prior notice or liability, under our sole
+          discretion, for any reason whatsoever and without limitation,
+          including but not limited to a breach of the Terms. Certain refund
+          requests for Subscriptions may be considered by Email Digest on a
+          case-by-case basis and granted in sole discretion of Email Digest.
+        </p>
+        */}
+        <p>
+          You may stop using our services at any time. Open AI also
+          reserves the right to terminate or suspend access to our services
+          immediately, without prior notice or liability, for any reason
+          whatsoever, including without limitation if you breach the Terms of
+          Service.
+        </p>
+        <h2 className="text-2xl font-bold ">Governing Law</h2>
+        <p>
+           These Terms of Service shall be governed
+          and construed in accordance with the laws of your jurisdiction and the
+          jurisdiction in which it operates, without regard to its conflict of
+          law provisions.
+        </p>
+        <h2 className="text-2xl font-bold ">Contact Us</h2>
+        <p>
+        If you have any questions about these
+          Terms of Service, please contact us at
+          support-email-digest@protonmail.com.
+        </p>
+        <p>
+          By using our services, you acknowledge that you have read and
+          understood these Terms of Service and agree to be bound by them. We
+          are committed to providing you with a reliable and useful service, and
+          we value your trust and privacy in our partnership.
         </p>
       </div>
     </>
