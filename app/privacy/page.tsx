3218e539164abebd20fc4bdb7d691c8c50a3189c
rodas-j <50768698+rodas-j@users.noreply.github.com>
2024-05-02 14:02:30 -0400

    Fix typo in privacy policy

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -227,7 +227,7 @@ export default function Privacy() {
           <strong>4. Limited Use</strong>
         </p>
         <p>
-          Email Digest&aposs use and transfer to any other app of information
+          Email Digest&apos;s use and transfer to any other app of information
           received from Google APIs will adhere to{" "}
           <a
             className="underline"
@@ -246,17 +246,17 @@ export default function Privacy() {
           the fact that the prompts you enter in Chat GPT are utilized by OpenAi
           to train the language model. On the other hand, Email Digest employs
           the API version of Chat GPT, which adheres to stricter data privacy
-          regulations. Please note that all data transmitted to Chat GPT&aposs
+          regulations. Please note that all data transmitted to Chat GPT&apos;s
           completion API endpoint is protected using the Transport Layer
           Security protocol while in transit. Additionally, any data sent to
-          Chat GPT&aposs completion API endpoint is kept private and used solely
-          by OpenAI to monitor potential misuse. This data is not for
+          Chat GPT&apos;s completion API endpoint is kept private and used
+          solely by OpenAI to monitor potential misuse. This data is not for
           developing, improving, or training generalized/ non-personalized AI/ML
           models. Furthermore, all transmitted information is automatically
           deleted after 30 days for added protection. For more information,
           please refer to{" "}
           <a className="underline" href="https://openai.com/enterprise-privacy">
-            OpenAI&aposs data usage policy
+            OpenAI&apos;s data usage policy
           </a>
           , specifically the section{" "}
           <b>Does OpenAI train its models on my business data?</b>
@@ -267,7 +267,7 @@ export default function Privacy() {
           </p>
           <p>We don’t store any of your emails in any of our databases.</p>
           <p>
-            We <span className="font-semibold">don&apost</span> use any of your
+            We <span className="font-semibold">don&apos;t</span> use any of your
             data to develop, improve, or train generalized/non-personalized AI
             and/or ML models.{" "}
           </p>
